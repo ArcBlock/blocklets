abe6f36ed979ff09968f07b8234cfff9c0a787fe
wangshijun <wangshijun2010@gmail.com>
2019-09-05 12:03:25 +0800

    chore: polish detail page

--- src/templates/blocklet/detail.js
+++ src/templates/blocklet/detail.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import useWindowSize from 'react-use/lib/useWindowSize';
+import useMeasure from 'react-use/lib/useMeasure';
 
 import withTheme from '@arcblock/ux/lib/withTheme';
 import withI18n from '@arcblock/www/components/withI18n';
@@ -17,79 +19,88 @@ import { ReactComponent as GithubLogo } from './images/github.svg';
 import { translations } from '../../libs/constant';
 import renderAst from '../../components/renderAst';
 
-class BlockletDetail extends React.PureComponent {
-  static propTypes = {
-    pageContext: PropTypes.object.isRequired,
-    location: PropTypes.object.isRequired,
-  };
+function BlockletDetail({ location, pageContext }) {
+  const { width: windowWidth } = useWindowSize();
+  const [ref, { width }] = useMeasure();
+  const {
+    name,
+    version,
+    provider,
+    keywords,
+    group,
+    htmlAst,
+    logoUrl,
+    gitUrl,
+  } = pageContext.blocklet;
 
-  render() {
-    const {
-      name,
-      version,
-      provider,
-      keywords,
-      group,
-      htmlAst,
-      logoUrl,
-      gitUrl,
-    } = this.props.pageContext.blocklet;
+  console.log({ width, windowWidth });
 
-    return (
-      <Layout location={this.props.location} title={name}>
-        <Div>
-          <div item xs={12} md={12} className={`header header--${group}`}>
-            <Container className="header__inner">
-              <Typography component="span" className="header__text">
-                <Typography component="span" variant="h2" className="header__text__title">
-                  Blocklet.
-                </Typography>
-                <Typography component="span" variant="h2" className="header__text__group">
-                  {group}
-                </Typography>
+  return (
+    <Layout location={location} title={name}>
+      <Div>
+        <div className={`header header--${group}`}>
+          <Container className="header__inner">
+            <Typography component="span" className="header__text" ref={ref}>
+              <Typography component="span" variant="h2" className="header__text__title">
+                Blocklet.
               </Typography>
-              <div component="span" className="header__logo">
-                <img src={logoUrl} className="header__logo__image" alt={name} />
-              </div>
-            </Container>
-          </div>
-          <div className="main">
-            <Container>
-              <div className="meta">
-                <Typography component="h2" variant="h2" className="title">
-                  {name}
-                  <Button href={gitUrl} target="_blank" className="github">
-                    <GithubLogo style={{ marginRight: 5 }} />
-                    View on Github
-                  </Button>
-                </Typography>
-                <Typography component="p" className="tags">
-                  <Tag className="tag" type="success">
-                    {provider}
-                  </Tag>
-                  <Tag className="tag" type="success">
-                    {version}
-                  </Tag>
-                  {Array.isArray(keywords) &&
-                    keywords.length > 0 &&
-                    keywords.map(keyword => <Tag className="tag">{keyword}</Tag>)}
-                </Typography>
-              </div>
-              <div className="markdown-body">
-                <Typography component="h2">Usage</Typography>
-                <CodeBlock>{`forge blocklet:use ${name}`}</CodeBlock>
-                <Typography component="h2">Documentation</Typography>
-                <PostContent component="div" className="content-wrapper post-content">
-                  {renderAst(htmlAst)}
-                </PostContent>
-              </div>
-            </Container>
-          </div>
-        </Div>
-      </Layout>
-    );
-  }
+              <Typography component="span" variant="h2" className="header__text__group">
+                {group}
+              </Typography>
+            </Typography>
+            <div
+              component="span"
+              className="header__logo"
+              style={{ width: (windowWidth - width) / 2 + 170, right: -(windowWidth - width) / 2 }}>
+              <img src={logoUrl} className="header__logo__image" alt={name} />
+            </div>
+          </Container>
+        </div>
+        <div className="main">
+          <Container>
+            <div className="meta">
+              <Typography component="h2" variant="h2" className="title">
+                {name}
+                <Button href={gitUrl} target="_blank" className="github">
+                  <GithubLogo style={{ marginRight: 5 }} />
+                  View on Github
+                </Button>
+              </Typography>
+              <Typography component="p" className="tags">
+                <Tag className="tag" type="success">
+                  {provider}
+                </Tag>
+                <Tag className="tag" type="success">
+                  {version}
+                </Tag>
+                {Array.isArray(keywords) &&
+                  keywords.length > 0 &&
+                  keywords.map(keyword => (
+                    <Tag className="tag" key={keyword}>
+                      {keyword}
+                    </Tag>
+                  ))}
+              </Typography>
+            </div>
+            <div className="markdown-body">
+              <Typography component="h2">Usage</Typography>
+              <CodeBlock>{`forge blocklet:use ${name}`}</CodeBlock>
+              <Typography component="h2">Documentation</Typography>
+              <PostContent component="div" className="content-wrapper post-content">
+                {renderAst(htmlAst)}
+              </PostContent>
+            </div>
+          </Container>
+        </div>
+      </Div>
+    </Layout>
+  );
 }
+
+BlockletDetail.propTypes = {
+  pageContext: PropTypes.object.isRequired,
+  location: PropTypes.object.isRequired,
+};
 
 export default withTheme(withI18n(translations)(BlockletDetail));
 
@@ -97,15 +108,15 @@ const codeFont = 'source-code-pro, Menlo, Monaco, Consolas, Courier New, monospa
 const Div = styled.div`
   .header {
     background-color: ${props => props.theme.palette.primary.light};
+    background-image: radial-gradient(
+      ${props => props.theme.palette.primary.main} 8%,
+      transparent 0
+    );
+    background-size: 20px 20px;
     height: 160px;
 
     .header__inner {
-      display: flex;
-      flex-direction: row;
-      justify-content: space-between;
-      @media (max-width: ${props => props.theme.breakpoints.values.sm}px) {
-        justify-content: flex-start;
-      }
+      position: relative;
     }
 
     .header__text {
@@ -113,6 +124,7 @@ const Div = styled.div`
       margin-bottom: 30px;
       display: flex;
       align-items: flex-end;
+      width: 100%;
     }
 
     .header__text__title {
@@ -132,12 +144,14 @@ const Div = styled.div`
     }
 
     .header__logo {
-      width: 170px;
+      box-sizing: border-box;
+      position: absolute;
       height: 160px;
       border-radius: 120px 0 0 120px;
+      padding-left: 50px;
       background-color: ${props => props.theme.palette.primary.main};
       display: flex;
-      justify-content: center;
+      justify-content: flex-start;
       align-items: center;
       @media (max-width: ${props => props.theme.breakpoints.values.sm}px) {
         display: none;
