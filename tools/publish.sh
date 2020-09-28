bcf83bda75496eaf6db860bdcb1a52e137675fcf
linchen1987 <linchen.1987@foxmail.com>
2020-09-28 20:33:58 +0800

    ci: auto send notification to slack; remove netily hook

--- tools/publish.sh
+++ tools/publish.sh
@@ -46,11 +46,26 @@ NODE_ENV=production abtnode bundle && npm publish _blocklet
 node tools/post-publish.js
 
 # deploy to remote ABT Node
+set +e
+NAME=$(cat package.json | grep name |  awk '{print $2}' | sed 's/"//g' | sed 's/,//g')
+VERSION=$(cat package.json | grep version |  awk '{print $2}' | sed 's/"//g' | sed 's/,//g')
 if [ "${ALIYUN_ENDPOINT}" != "" ]; then
   abtnode deploy . --endpoint ${ALIYUN_ENDPOINT} --access-key ${ALIYUN_ACCESS_KEY} --access-secret ${ALIYUN_ACCESS_SECRET} --skip-hooks
-  echo "deploy to ${ALIYUN_ENDPOINT} success"
+  if [ $? == 0 ]; then
+    echo "deploy to ${ALIYUN_ENDPOINT} success"
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${NAME} v${VERSION} was successfully deployed to ${ALIYUN_ENDPOINT}\"}" ${SLACK_WEBHOOK}
+  else
+    echo "deploy to ${ALIYUN_ENDPOINT} failed"
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\":x: Faild to deploy ${NAME} v${VERSION} to ${ALIYUN_ENDPOINT}\"}" ${SLACK_WEBHOOK}
+  fi
 fi
 if [ "${AWS_ENDPOINT}" != "" ]; then
   abtnode deploy . --endpoint ${AWS_ENDPOINT} --access-key ${AWS_ACCESS_KEY} --access-secret ${AWS_ACCESS_SECRET} --skip-hooks
-  echo "deploy to ${ALIYUN_ENDPOINT} success"
+  if [ $? == 0 ]; then
+    echo "deploy to ${AWS_ENDPOINT} success"
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${NAME} v${VERSION} was successfully deployed to ${AWS_ENDPOINT}\"}" ${SLACK_WEBHOOK}
+  else
+    echo "deploy to ${AWS_ENDPOINT} failed"
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\":x: Faild to deploy ${NAME} v${VERSION} to ${AWS_ENDPOINT}\"}" ${SLACK_WEBHOOK}
+  fi
 fi
