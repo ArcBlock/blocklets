a9f59d7cc27caa9689b643bcf34e5b285bbb315a
linchen1987 <linchen.1987@foxmail.com>
2020-09-28 20:35:39 +0800

    ci: auto send notification to slack; remove netily hook (#61)

--- tools/post-publish.js
+++ tools/post-publish.js
@@ -30,13 +30,4 @@ const sendAlerts = packageDirs => {
   });
 };
 
-(async () => {
-  try {
-    const res = await axios.post('https://api.netlify.com/build_hooks/5d71fd6472feae0bb5d28671');
-    console.log('trigger blocklets build success:', res.status);
-  } catch (error) {
-    console.error('trigger blocklets build failed:', error);
-  }
-})();
-
 sendAlerts(['.']);
