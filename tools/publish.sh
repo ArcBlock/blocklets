1d967f503950438cec54dab63af0f94d1a496e23
linchen1987 <linchen.1987@foxmail.com>
2020-11-11 18:21:33 +0800

    [skip ci] update ci

--- tools/publish.sh
+++ tools/publish.sh
@@ -57,23 +57,23 @@ node tools/post-publish.js
 set +e
 NAME=$(cat package.json | grep name |  awk '{print $2}' | sed 's/"//g' | sed 's/,//g')
 VERSION=$(cat package.json | grep version |  awk '{print $2}' | sed 's/"//g' | sed 's/,//g')
-if [ "${ALIYUN_ENDPOINT}" != "" ]; then
-  abtnode deploy .blocklet/bundle --endpoint ${ALIYUN_ENDPOINT} --access-key ${ALIYUN_ACCESS_KEY} --access-secret ${ALIYUN_ACCESS_SECRET} --skip-hooks
+if [ "${ALIYUN_NODE_ENDPOINT}" != "" ]; then
+  abtnode deploy .blocklet/bundle --endpoint ${ALIYUN_NODE_ENDPOINT} --access-key ${ALIYUN_NODE_ACCESS_KEY} --access-secret ${ALIYUN_NODE_ACCESS_SECRET} --skip-hooks
   if [ $? == 0 ]; then
-    echo "deploy to ${ALIYUN_ENDPOINT} success"
-    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${NAME} v${VERSION} was successfully deployed to ${ALIYUN_ENDPOINT}\"}" ${SLACK_WEBHOOK}
+    echo "deploy to ${ALIYUN_NODE_ENDPOINT} success"
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${NAME} v${VERSION} was successfully deployed to ${ALIYUN_NODE_ENDPOINT}\"}" ${SLACK_WEBHOOK}
   else
-    echo "deploy to ${ALIYUN_ENDPOINT} failed"
-    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\":x: Faild to deploy ${NAME} v${VERSION} to ${ALIYUN_ENDPOINT}\"}" ${SLACK_WEBHOOK}
+    echo "deploy to ${ALIYUN_NODE_ENDPOINT} failed"
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\":x: Faild to deploy ${NAME} v${VERSION} to ${ALIYUN_NODE_ENDPOINT}\"}" ${SLACK_WEBHOOK}
   fi
 fi
-if [ "${AWS_ENDPOINT}" != "" ]; then
-  abtnode deploy .blocklet/bundle --endpoint ${AWS_ENDPOINT} --access-key ${AWS_ACCESS_KEY} --access-secret ${AWS_ACCESS_SECRET} --skip-hooks
+if [ "${AWS_NODE_ENDPOINT}" != "" ]; then
+  abtnode deploy .blocklet/bundle --endpoint ${AWS_NODE_ENDPOINT} --access-key ${AWS_NODE_ACCESS_KEY} --access-secret ${AWS_NODE_ACCESS_SECRET} --skip-hooks
   if [ $? == 0 ]; then
-    echo "deploy to ${AWS_ENDPOINT} success"
-    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${NAME} v${VERSION} was successfully deployed to ${AWS_ENDPOINT}\"}" ${SLACK_WEBHOOK}
+    echo "deploy to ${AWS_NODE_ENDPOINT} success"
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${NAME} v${VERSION} was successfully deployed to ${AWS_NODE_ENDPOINT}\"}" ${SLACK_WEBHOOK}
   else
-    echo "deploy to ${AWS_ENDPOINT} failed"
-    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\":x: Faild to deploy ${NAME} v${VERSION} to ${AWS_ENDPOINT}\"}" ${SLACK_WEBHOOK}
+    echo "deploy to ${AWS_NODE_ENDPOINT} failed"
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\":x: Faild to deploy ${NAME} v${VERSION} to ${AWS_NODE_ENDPOINT}\"}" ${SLACK_WEBHOOK}
   fi
 fi
